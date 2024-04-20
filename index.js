37ce7c1aa704aa85d6542f92f90bb10b348cb74f
Eren <114621741+k-eren-k@users.noreply.github.com>
2024-04-20 19:01:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs'); 
 app.set('views', './views'); 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
